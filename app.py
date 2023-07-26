df287c257c72a196f6b8b77b815347a83b6b3fad
Ubuntu <ubuntu@ip-172-31-95-80.ec2.internal>
2023-07-26 23:49:01 +0000

    fix: update app endpoints and add env to gitignore

--- app.py
+++ app.py
@@ -6,25 +6,25 @@ import mlflow
 from flask import Flask, request
 from mlflow.tracking import MlflowClient
 
-
-config = dotenv_values(".env")
 logger = logging.getLogger(__name__)
-
-TRACKING_SERVER_HOST = config.get(
-    "TRACKING_SERVER_HOST", "test_tracking_server"
-)  # public DNS of the EC2 instance
-
-client = MlflowClient(tracking_uri=f"http://{TRACKING_SERVER_HOST}:5000")
-mlflow.set_tracking_uri(f"http://{TRACKING_SERVER_HOST}:5000")
-
-RUN_ID = client.search_registered_models()[0].latest_versions[0].run_id
-logged_model = f"s3://mlflow-artifact-remote-isa/3/{RUN_ID}/artifacts/models/"
-
-model = mlflow.sklearn.load_model(logged_model)
+config = dotenv_values(".env")
 
 
 def predict(input_value):
     """Obtain the model's inference from the given input."""
+    logger.info(f"CONFIGS: {config}")
+    TRACKING_SERVER_HOST = config.get(
+        "TRACKING_SERVER_HOST", "test_tracking_server"
+    )  # public DNS of the EC2 instance
+
+    client = MlflowClient(tracking_uri=f"http://{TRACKING_SERVER_HOST}:5000")
+    mlflow.set_tracking_uri(f"http://{TRACKING_SERVER_HOST}:5000")
+
+    RUN_ID = client.search_registered_models()[0].latest_versions[0].run_id
+    logged_model = f"s3://mlflow-artifact-remote-isa/3/{RUN_ID}/artifacts/models/"
+
+    model = mlflow.sklearn.load_model(logged_model)
+
     logger.info("Predicting: %s", str(input_value))
 
     if isinstance(input_value, dict):
@@ -35,16 +35,25 @@ def predict(input_value):
     return predictions.tolist()
 
 
-app = Flask("laundering-model-prediction")
+app = Flask(__name__)
 
 
 @app.route("/predict", methods=["POST"])
 def predict_endpoint():
+    logger.info(f"Route predict")
     features = request.get_json()
+    logger.info(f"FEATURES: {features}")
     pred = predict(features)
     return pred
 
 
+@app.route("/")
+def hello_world():
+    logger.info("Route hello world")
+    logger.info(f"CONFIGS: {config}")
+    return "Holi Dani! Te envío un abrazo digital"
+
+
 if __name__ == "__main__":
-    # app.run(debug=True, host='0.0.0.0', port=9696)
+    #app.run(debug=True, host="0.0.0.0", port=9696)
     app.run()
